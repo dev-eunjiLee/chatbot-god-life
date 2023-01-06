25e4e60b61f546a236198265b42c7769e475b4bd
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-07 00:23:03 +0900

    FIX: (user) userList를 이용해 계정 생성 로직 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserInputDto } from './dtos/createUser.dto';
+import { userList } from './table/user';
 
 @Controller('user')
 export class UserController {
   @Post('/create')
   createUser(@Body() createUserInputDto: CreateUserInputDto) {
     console.log(createUserInputDto);
+    userList.push(createUserInputDto);
+    console.log(userList);
   }
 }
