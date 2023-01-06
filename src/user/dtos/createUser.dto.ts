9394d6f2f891cfbb204f5d0b0b1452526987b382
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-07 00:17:44 +0900

    FIX: (user) 계정 생성 정보 validation 추가

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,13 +1,15 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 import { DEFAULT_GOAL, DEFAULT_NICKNAME } from '../consts/defaultValue';
 import { GOAL, NICKNAME } from '../consts/type';
 
 export class CreateUserInputDto {
   // 유저 이름
   @IsString()
+  @Length(0, 10)
   nickname: NICKNAME = DEFAULT_NICKNAME;
 
   // 유저 목표
   @IsString()
+  @Length(0, 20)
   goal: GOAL = DEFAULT_GOAL;
 }
