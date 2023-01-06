9c4ddca7155954cb7d07bfbe2d930924e52fc33b
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-07 00:15:00 +0900

    FIX: (user) 계정 생성 시 디폴트 및 타입 사용

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,11 +1,13 @@
 import { IsString } from 'class-validator';
+import { DEFAULT_GOAL, DEFAULT_NICKNAME } from '../consts/defaultValue';
+import { GOAL, NICKNAME } from '../consts/type';
 
 export class CreateUserInputDto {
   // 유저 이름
   @IsString()
-  nickname: string;
+  nickname: NICKNAME = DEFAULT_NICKNAME;
 
   // 유저 목표
   @IsString()
-  goal: string;
+  goal: GOAL = DEFAULT_GOAL;
 }
