18918e0d9e72bccdaa2b3359e233d82425083b2e
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-21 23:30:54 +0900

    FIX: 짝수인 경우 지연평가 할 수 있도록 제너레이터 함수 사용 추가, take 함수 추가

--- src/user/services/get-user-list.service.ts
+++ src/user/services/get-user-list.service.ts
@@ -20,20 +20,15 @@ export class GetUserListService implements GetUserListInboundPort {
     params: GetUserListInboundPortInputDto,
   ): Promise<GetUserListInboundPortOutputDto> {
     // 함수형 프로그래밍 테스트를 위해 DB에서 전체 데이터를 가져온 후 필터링
-    const userList = await this.getUserListOutboundPort.execute();
+    const originUserList = await this.getUserListOutboundPort.execute();
 
     // 지연 평가 구현을 위해 요리조리 제너레이터 사용해보는 중
-    const generatorUserList = this.makeUserIterable(userList);
-    const evenUserList = this.makeEvenUserIterable(generatorUserList);
-    for (const user of evenUserList) {
-      console.log(user);
-    }
+    const generatorUserList = this.makeUserIterable(originUserList);
 
-    console.log(userList);
-    console.log(generatorUserList);
-    console.log(evenUserList);
-
-    return userList;
+    // 아이디가 짝수인 경우
+    const evenUserList = this.makeEvenIdUserList(generatorUserList);
+    // 최신순으로 일정 개수만큼 가져오기
+    return this.take(evenUserList, params.length);
   }
 
   private *makeUserIterable(userList: Array<User>): IterableIterator<User> {
@@ -44,19 +39,23 @@ export class GetUserListService implements GetUserListInboundPort {
     }
   }
 
-  private *makeEvenUserIterable(
-    genUser: IterableIterator<User>,
+  private *makeEvenIdUserList(
+    genUserList: IterableIterator<User>,
   ): IterableIterator<User> {
-    let flag = true;
-    let i = 0;
-    while (flag) {
-      const user = genUser.next().value as User;
-      if (user === undefined) {
-        flag = false;
-      } else if (user.id % 2 === 0) {
-        yield user;
-      }
-      i++;
+    for (const user of genUserList) {
+      if (user.id % 2 === 0) yield user;
     }
+  }
+
+  private take(userList: IterableIterator<User>, length = 1): Array<User> {
+    let i = 0;
+    const takeUserList: Array<User> = [];
+    for (const user of userList) {
+      if (i < length) {
+        takeUserList.push(user);
+        i++;
+      } else break;
+    }
+    return takeUserList;
   }
 }
