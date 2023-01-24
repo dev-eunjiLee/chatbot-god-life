ab1df8a28134e868cd1f25fc592084df687306b5
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-24 22:40:23 +0900

    FIX: 카카오톡 response 타입에 맞춰서 테스트 진행

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('test')
-  test(): string {
-    return 'Hello';
+  @Post('test')
+  test(): { version: string; data: { msg: string } } {
+    return {
+      version: '2.0',
+      data: {
+        msg: 'test',
+      },
+    };
   }
   @Get()
   getHello(): string {
