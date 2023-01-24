87f30e262d3d839ff8ea401f94545513ad2db83b
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-24 23:37:35 +0900

    FIX: 카카오 input 받아서 output 리턴하는 테스트 함수 완성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Post, Get, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { KakaoSkillInputDto } from './common/dtos/kakao.skill.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('test')
-  test(@Body() body: any): { version: string; data: { msg: string } } {
+  test(@Body() body: KakaoSkillInputDto): any {
     console.log(body);
     return {
       version: '2.0',
-      data: {
-        msg: 'test',
+      template: {
+        outputs: [
+          {
+            simpleText: body.userRequest.utterance,
+          },
+        ],
       },
     };
   }
