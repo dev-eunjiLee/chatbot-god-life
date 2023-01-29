d58eb77bfb7171245b215a298bcb8c264393b439
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-30 00:54:07 +0900

    FIX: (DB) Mysql 연결할 때, config 입력할 수 있도록 로직 작성중

--- src/lib/mysql/mysql.module.ts
+++ src/lib/mysql/mysql.module.ts
@@ -1,5 +1,6 @@
-import { DynamicModule, Global, Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import mysql, { Connection } from 'mysql2/promise';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 export interface MysqlConnectConfig {
   host: string;
@@ -14,18 +15,39 @@ export const MYSQL_CONNECTION = 'MYSQL_CONNECTION';
 export const createConnection = async (
   config: MysqlConnectConfig,
 ): Promise<Connection> => {
+  console.log(mysql);
   return mysql.createConnection(config);
+};
+
+const connectionConfig = async (
+  configService: ConfigService,
+): Promise<MysqlConnectConfig> => {
+  const configKeyList = ['host', 'user', 'password', 'database'];
+  const configMap = new Map<string, string>([
+    ['host', 'MYSQL_HOST'],
+    ['user', 'MYSQL_USER'],
+    ['password', 'MYSQL_PASSWORD'],
+    ['database', 'MYSQL_DATABASE'],
+  ]);
+  const config: { [key: string]: string } = {};
+  for (const key of configKeyList) {
+    const configKey = configMap.get(key) as string;
+    config[key] = (await configService.get(configKey)) as string;
+  }
+  return config as unknown as MysqlConnectConfig;
 };
 
 @Module({})
 export class MysqlModule {
-  static forRoot(config: MysqlConnectConfig): DynamicModule {
+  static async forRootAsync(): Promise<DynamicModule> {
     return {
       module: MysqlModule,
+
       providers: [
         {
           provide: MYSQL_CONNECTION_CONFIG,
-          useValue: config,
+          inject: [ConfigService],
+          useFactory: connectionConfig,
         },
         {
           provide: MYSQL_CONNECTION,
@@ -34,7 +56,7 @@ export class MysqlModule {
             await createConnection(config),
         },
       ],
-      imports: [],
+      imports: [ConfigModule],
       exports: [],
     };
   }
