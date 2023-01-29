2fba86344fd655b76abec727468af0a429246ce3
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-29 22:52:29 +0900

    FIX: (유저) 계정 생성 컨트롤러 모듈에 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CREATE_USER_INBOUND_PORT } from './inbound-port/create-user.inbound-port';
 import { CreateUserService } from './services/create-user.service';
+import { CreateUserController } from './controllers/create-user.controller';
 
 @Module({
+  controllers: [CreateUserController],
   providers: [
     {
       provide: CREATE_USER_INBOUND_PORT,
