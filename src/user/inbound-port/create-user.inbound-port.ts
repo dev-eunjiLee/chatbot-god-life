9ed5c2fb7f5b6c784936d5cd678a13e045279d07
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-29 22:48:09 +0900

    FIX: (유저) 모듈 등록용 토큰 추가

--- src/user/inbound-port/create-user.inbound-port.ts
+++ src/user/inbound-port/create-user.inbound-port.ts
@@ -1,5 +1,7 @@
 import { KakaoSkillInputDto } from '../../common/dtos/kakao.skill.dto';
 
+export const CREATE_USER_INBOUND_PORT = 'CREATE_USER_INBOUND_PORT';
+
 export type CreateUserInboundPortInputDto = KakaoSkillInputDto;
 export type CreateUserInboundPortOutputDto = void;
 
