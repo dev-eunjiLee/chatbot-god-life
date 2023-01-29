08e843f291e98b4b1e18d82b3912897d8092192a
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-29 17:32:57 +0900

    FIX: (계정 생성) input 타입을 카카오 스킬 인풋 타입으로 지정

--- src/user/inbound-port/create-user.inbound-port.ts
+++ src/user/inbound-port/create-user.inbound-port.ts
@@ -1,4 +1,6 @@
-export type CreateUserInboundPortInputDto = void;
+import { KakaoSkillInputDto } from '../../common/dtos/kakao.skill.dto';
+
+export type CreateUserInboundPortInputDto = KakaoSkillInputDto;
 export type CreateUserInboundPortOutputDto = void;
 
 export interface CreateUserInboundPort {
