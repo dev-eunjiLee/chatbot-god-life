707d9aec6fca63fcd1b48366429d514171d7b126
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-29 22:48:34 +0900

    FIX: (유저) 모듈에 유저 생성 서비스 클래스 등록

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CREATE_USER_INBOUND_PORT } from './inbound-port/create-user.inbound-port';
+import { CreateUserService } from './services/create-user.service';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: CREATE_USER_INBOUND_PORT,
+      useClass: CreateUserService,
+    },
+  ],
+})
 export class UserModule {}
