0ecb74fc129bca7ce4907c1da615810153c19c46
dev-EunjiLee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-30 00:53:27 +0900

    FIX: (유저) 환경변수, MysqlModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { MysqlModule } from './lib/mysql/mysql.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env.dev' }), UserModule],
+  imports: [
+    // 환경설정
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_USER: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+    // DB 설정
+    MysqlModule.forRootAsync(),
+    // 서비스 설정
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
